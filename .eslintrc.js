9f133339f8851d652333a5336b5e5d409235e9ae
neverbiasu <1751162157@qq.com>
2025-08-30 20:52:11 +0800

    chore(ci-cd): set NODE_ENV to development for dependency installation steps

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
     'prettier'
   ],
   plugins: ['@typescript-eslint', 'prettier'],

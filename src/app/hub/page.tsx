24a4f0d8460e437eb9663ede3d55094c2f2d007b
neverbiasu <1751162157@qq.com>
2025-12-23 13:46:47 +0000

    feat(web): add contest hub page logic

---
+++ src/app/hub/page.tsx
@@ -0,0 +1,13 @@
+import { getAllContests } from '@/lib/data';
+import { HubClient } from './client';
+
+// Server Component
+export default async function HubPage() {
+  const initialContests = await getAllContests();
+  
+  return (
+    <div className="min-h-screen bg-canvas font-mono flex flex-col">
+       <HubClient initialContests={initialContests} />
+    </div>
+  );
+}
